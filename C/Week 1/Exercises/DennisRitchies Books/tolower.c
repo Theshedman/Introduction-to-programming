ceb190caeb99c73e210cbd24693634cac9782142
Oromion <caznaranl@uni.pe>
2017-09-13 20:34:02 -0500

    Add files via upload

---
+++ C/Week 1/Exercises/DennisRitchies Books/tolower.c
@@ -0,0 +1,17 @@
+#include <stdio.h> /*comentarios*/
+#include <ctype.h>
+
+int main()
+{
+    int i=0;
+    char c;
+    char str[] = "Oromion GitHub";
+
+    while ( str[i] )
+    {
+        putchar(tolower(str[i]));
+        i++;
+    }
+
+    return (0);
+}
