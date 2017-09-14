e43ab030a84143e23f0126093739037c85a79ee3
Oromion <caznaranl@uni.pe>
2017-09-13 20:37:06 -0500

    Add files via upload

---
+++ C/Week 1/Exercises/Various/factorial.c
@@ -0,0 +1,23 @@
+#include<stdio.h>/*comentario de prueba3*/
+int main()
+{
+    int n,i;
+    unsigned long long factorial =1;
+
+    printf("Ingrese un entero: ");
+    scanf("%d",&n);
+
+    // mostrar error si el usuario ingresa un entero negativo
+    if (n < 0)
+        printf("Error! Factorial de un número negativo no existe.");
+
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;             //factorial = factorial*i
+        }
+        printf("Factorial de %d = %llu", n, factorial);
+    }
+  return 0;
+}
